82201ccd4870fbfb3824cfc7af89641fc62e9541
pedro gonzales <pedrogonzals@gmail.com>
2014-12-18 11:34:11 -0800

    my site - django

---
+++ polls/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import HttpResponse, render
+
+from polls.models import Poll
+# Create your views here.
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
